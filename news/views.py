f70468459b72a29ac56b642961d6681a4ba4e603
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-12 11:05:08 +0300

    Sending a Welcome email created a Newsletter,its model and associated templates

--- news/views.py
+++ news/views.py
@@ -4,9 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 
 
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 import datetime as dt
 from .models import Article
+from .forms import NewsLetterForm
+from .email import send_welcome_email
 
 # Create your views here.
 def welcome(request):
@@ -68,7 +70,23 @@ def news_today(request):
     date = dt.date.today()
     news = Article.today_news()
 
-    return render(request, 'all-news/today-news.html', {"date":date,"news":news})
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            # print('valid')
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = NewsLetterRecipients(name = name, email = email)
+            recipient.save()
+            send_welcome_email(name, email)
+
+            HttpResponseRedirect('news_today')
+    else:
+        form = NewsLetterForm()
+
+    return render(request, 'all-news/today-news.html', {"date":date, "news":news, "letterForm":form})
+
 
 
 def search_results(request):
