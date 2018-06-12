7f0126c0c7e29d9d7385165820a9e734e47085fb
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-12 16:24:13 +0300

    removed registration of the Editor Model

--- news/admin.py
+++ news/admin.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Editor, Article, tag
+# from .models import Editor, Article, tag
+from .models import Article, tag
+
 
 # Register your models here.
 
@@ -10,6 +12,6 @@ class ArticleAdmin(admin.ModelAdmin):
     filter_horizontal =('tag',)
 
 
-admin.site.register(Editor)
+# admin.site.register(Editor)
 admin.site.register(Article)
 admin.site.register(tag)
