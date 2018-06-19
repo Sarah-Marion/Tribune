5524e4001596207f99fddb3a008777a976a4c942
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-19 10:54:29 +0300

    built an api

--- news/views.py
+++ news/views.py
@@ -10,6 +10,10 @@ from .models import Article, NewsLetterRecipients
 from .forms import NewsLetterForm, NewsArticleForm
 from .email import send_welcome_email
 from django.contrib.auth.decorators import login_required
+from rest_framework.response import Response 
+from rest_framework.views import APIView
+from .models import MoringaMerch
+from .serializer import MerchSerializer
 
 
 # Create your views here.
@@ -139,3 +143,10 @@ def newsletter(request):
     send_welcome_email(name, email)
     data = {'success':'You have been sucessfully added to mailing list'}
     return JsonResponse(data)
+
+
+class MerchList(APIView):
+    def get(self, request, format=None):
+        all_merch = MoringaMerch.objects.all()
+        serializer = MerchSerializer(all_merch, many=True)
+        return Response(serializer.data)
