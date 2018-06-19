60a59edc421bf60d97bb12c833bf483ef85b85dc
Sarah-Marion <devsarahmarion@gmail.com>
2018-06-19 22:56:10 +0300

    getting a single item created APIView to handle request for fetching a single id created a Url configuration to act as an endpoint for a single item updated single item created a function for admin to delete objects

--- news/views.py
+++ news/views.py
@@ -15,6 +15,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from .models import MoringaMerch
 from .serializer import MerchSerializer
+from permissions import IsAdminOrReadOnly
 
 
 # Create your views here.
@@ -154,7 +155,41 @@ class MerchList(APIView):
 
     def post(self, request, format=None):
         serializer = MerchSerializer(data = request.data)
+        
+        permission_classes = (IsAdminOrReadOnly,)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class MerchDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+
+    def get_merch(self, pk):
+        try:
+            return MoringaMerch.objects.get(pk=pk)
+        except MoringaMerch.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        merch = self.get_merch(pk)
+        serializer = MerchSerializer(merch)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        merch = self.get_merch(pk)
+        serializer = MerchSerializer(merch, request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        merch = self.get_merch(pk)
+        merch.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
